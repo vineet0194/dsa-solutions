e67496b70d4ceefcbe725240104773c7bacf5e4e
Vineet Singh <vineet.singh0194@gmail.com>
2026-07-15 08:09:19 +0530

    Solved Leetcode POTD 15 Jul 2026

---
+++ z_Leetcode/POTD/2026/7_July/15_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,17 @@
+/*
+Problem: 3658. GCD of Odd and Even Sums
+Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/?envType=daily-question&envId=2026-07-15
+Difficulty: Easy
+Approach: Math, Euclidean Algos
+Time Complexity: O(1)
+Note:
+    expl:
+    https://leetcode.com/problems/gcd-of-odd-and-even-sums/solutions/8397778/beats-100-1-line-math-proof-gcd-simplifi-q3hw
+*/
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};

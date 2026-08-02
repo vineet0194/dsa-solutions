ef508330c79e131ea41a4de0d3b0f449081fbe1f
Vineet Singh <vineet.singh0194@gmail.com>
2026-08-02 12:55:46 +0530

    Solved Leetcode POTD 2 Aug 2026

---
+++ z_Leetcode/POTD/2026/8_August/2_Stone_Game.cpp
@@ -0,0 +1,16 @@
+/*
+Problem: 877. Stone Game
+Link: https://leetcode.com/problems/stone-game/?envType=daily-question&envId=2026-08-02
+Difficulty: Medium
+Approach: Logic
+Time Complexity: O(1)
+Note:
+    Since the number of piles is even and both players play optimally, the first player can always choose to pick either all even-indexed or all odd-indexed piles, ensuring a win.
+*/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};

849dc3fd0bef4167cca817def6eebb9a36f317b1
Vineet Singh <vineet.singh0194@gmail.com>
2026-09-07 22:28:18 +0530

    Solved Leetcode POTD 7 Sep 2026

---
+++ z_Leetcode/POTD/2026/9_September/7_Distinct_Subsequences_2.cpp
@@ -0,0 +1,51 @@
+/*
+Problem: 940. Distinct Subsequences II
+Link: https://leetcode.com/problems/distinct-subsequences-ii/?envType=daily-question&envId=2026-09-07
+Difficulty: Hard
+Approach: DP
+Time Complexity: O(n)
+Note:
+    
+*/
+
+class Solution {
+public:
+    int M = 1e9+7;
+    int dp[2001];
+    vector<int> prev; //prev[n] = last time when we saw this nth character (1-based indexing)
+
+    int solve(int n) {
+        if(n == 0)
+            return 1;
+
+        if(dp[n] != -1)
+            return dp[n];
+        
+        int total = (2*solve(n-1)) % M;
+
+        if(prev[n] != 0) {
+            int duplicates = solve(prev[n] - 1);
+            total = (total - duplicates + M) % M;
+        }
+
+        return dp[n] = total;
+    }
+
+    int distinctSubseqII(string s) {
+        int n = s.length();
+
+        memset(dp, -1, sizeof(dp));
+        prev.assign(n+1, 0);
+
+        vector<int> lastSeen(26, 0);
+        for(int i = 1; i <= n; i++) {
+            int idx = s[i-1] -'a';
+
+            prev[i] = lastSeen[idx];
+            lastSeen[idx] = i;
+        }
+
+        return (solve(n) - 1 + M) % M;
+
+    }
+};

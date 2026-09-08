e9769f4194c5b75898e2c16116078b531200bee7
Vineet Singh <vineet.singh0194@gmail.com>
2026-09-08 22:14:44 +0530

    Solved Leetcode POTD 8 Sep 2026

---
+++ z_Leetcode/POTD/2026/9_September/8_Count_Commas_in_Range.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: 3870. Count Commas in Range
+Link: https://leetcode.com/problems/count-commas-in-range/?envType=daily-question&envId=2026-09-08
+Difficulty: Easy
+Approach: Logic
+Time Complexity: O(1)
+Note:
+    
+*/
+
+class Solution {
+public:
+    int countCommas(int n) {
+        if (n < 1000)
+            return 0;
+        
+        if (n <= 10000)
+            return n-1000+1;
+        
+        return n-1000+1;
+    }
+};
